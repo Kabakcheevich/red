d9f184520c48e7a82451c7e42e6e1a5a1c4e4190
Dmytro Kabakchei <dkabakchei@edu.cdv.pl>
2024-05-22 23:24:52 +0200

    Доделал рычание.
    Закончил с рычанием. Пиздец, я так закончу этот проект через год

--- Assets/Scenes/scripts/BearController.cs
+++ Assets/Scenes/scripts/BearController.cs
@@ -6,11 +6,7 @@ public class BearController : MonoBehaviour
 {
     public Animator animator;
     public float detectionRange = 2.0f;
-    public string BuffAnimation = "Buff";
     public GameObject Skull;
-
-    // ƒобавл€ем параметр, который будет управл€ть переходами анимации
-    private bool isBuff = false;
 
     void Start()
     {
@@ -23,23 +19,13 @@ public class BearController : MonoBehaviour
         {
             float distance = Vector3.Distance(transform.position, Skull.transform.position);
 
-            // ѕровер€ем рассто€ние и устанавливаем параметр "isBuff"
             if (distance < detectionRange)
             {
-                isBuff = true;
+                animator.SetBool("IsBuff", true);
             }
             else
             {
-                isBuff = false;
-            }
-
-            // ”станавливаем параметр "IsBuff" в аниматоре
-            animator.SetBool("isBuff", isBuff);
-
-            // ѕроигрываем анимацию "Buff" при условии, что "isBuff" == false
-            if (!isBuff)
-            {
-                animator.Play(BuffAnimation);
+                animator.SetBool("IsBuff", false);
             }
         }
     }
